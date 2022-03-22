7a41d5e89fec9e157b2f60bacb2b85d52d7f1bc7
kesavgupta <101472174+kesavgupta@users.noreply.github.com>
2022-03-22 11:46:11 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,147 +1,130 @@
 #include <kipr/wombat.h>
-
+void SERVO_RIGHT();
+void SERVO_LEFT();
+void BABYWIPES();
+void DECAPITATERUP();
+void DECAPITATERDOWN();
+void WINDSHEILDWIPPERYAY();
 int main()
 {
-    int STAGE = 1;
-    int rc = 0;
-    // range count
-    //set to starting pos
-    enable_servos();
-    set_servo_position(0,1900);
-    set_servo_position(1,1100);
-    set_servo_position(2,801);
-    msleep(150);
-    disable_servos();
-    //stop at ring
-
-
-    motor(2,30);
-    motor(3,30);
+    create_connect();
+    DECAPITATERUP();
+    msleep(1000);
+    create_drive_direct(-100,-100);
+    msleep(500);
+    create_stop();
+    DECAPITATERDOWN();
+    create_drive_direct(100,100);
+    msleep(500);
+     create_stop();
+    create_drive_direct(100,-100);
+    msleep(650);
+     create_stop();
+    DECAPITATERUP();
+    msleep(1000);
+    create_drive_direct(-100,-100);
     msleep(3000);
-
-    motor(2,0);
-    motor(3,0);
-    msleep(400);
-    STAGE = 2;
-
-
-
-    if(STAGE==2)
-    {
-
-        // move forward
-        motor(2,30);
-        motor(3,30);
-        msleep(1000);
-
-
-        STAGE = 3;
-    }
-    // turn
-    while(STAGE == 3)
-    {
-        if(analog(3)<1200)
-        {
-            // turn
-            motor(2,30);  
-            motor(3,-30);
-        }
-        else
-        {
-            motor(2,30);  
-            motor(3,-30);
-            msleep(1300);
-            motor(2,0);  //
-            motor(3,0);
-            msleep(100);
-            motor(2,40);
-            motor(3,40);
-            msleep(200);
-            STAGE = 4;
-        }
-    }
-
-    // forward
-
-    if(STAGE == 4) //
-    {
-        motor(2,40);
-        motor(3,40);
-        msleep(5000);
-        // revrse + line up
-        motor(2,-40);
-        motor(3,-40);
-        msleep(2500);
-        motor(2,40);
-        motor(3,-40);
-        msleep(3000);
-        motor(2,-40);
-        motor(3,-40);
-        msleep(2700);
-        STAGE = 5;
-    }
-    while(STAGE == 5)
-    {
-        enable_servos();
-        set_servo_position(1,1450);
-        set_servo_position(2,520);
-        msleep(150);
-        disable_servos();
-        STAGE = 6;
-    }
-    // finding range stage
-    while(STAGE == 6)
-    {
-        if(49>=rc || rc >=0)
-        {
-            motor(2,-10);
-            motor(3,-10);
-            if(analog(0)>1500)
-            {
-                motor(2,-10);
-                motor(3,-10);
-                msleep(200);
-                STAGE = 7;
-            }
-            else
-            {
-                rc = rc + 1;
-                msleep(30);
-            }
-        }
-        if(rc>= 50)
-        {
-            motor(2,10);
-            motor(3,10);
-            if(analog(0)>1500)
-            {
-                motor(2,10);
-                motor(3,10);
-                msleep(300);
-                STAGE = 7;
-            }
-            else
-            {
-                rc = rc - 1;
-                msleep(5);
-            }
-        }
-    }
-    if(STAGE == 7)
-    {
-        motor(2,0);
-        motor(3,0);
-        msleep(10);
-        enable_servos();
-        set_servo_position(0,1900);
-        set_servo_position(1,1100);
-        set_servo_position(2,801);
-        msleep(150);
-        disable_servos();
-    }
-
-
-
-    printf("Hello World\n");
-    return 0;
+     create_stop();
+     DECAPITATERDOWN();
+    create_drive_direct(100,100);
+    msleep(1000);
+    create_stop();
+   create_drive_direct(100,-100);
+    msleep(640);
+    create_stop();
+    DECAPITATERUP();
+     create_drive_direct(-100,-100);
+    msleep(1000);
+     create_stop();
+    create_drive_direct(-100,0);
+    msleep(500);
+     create_stop();
+     create_drive_direct(-100,-100);
+    msleep(1200);
+    create_stop();
+    create_drive_direct(-9,-90);
+    msleep(1000);
+    DECAPITATERDOWN();
+    SERVO_RIGHT();
+     create_drive_direct(100,100);
+    msleep(500);
+    create_stop();
+    DECAPITATERUP();
+     create_drive_direct(-100,-100);
+    msleep(1000);
+    create_stop();
+    SERVO_LEFT();
+    msleep(1111);
+    create_disconnect();
+        return 0;
 }
+void SERVO_RIGHT()
+{
+    int stepper=0;
+     enable_servos();
+    while (stepper<1453)
+    {
+       
+        set_servo_position(0,stepper);
+        stepper=stepper+10;
+        msleep(20);
+    }
+     disable_servos();
+}
+void SERVO_LEFT()
+{
+    int stepper=1453;
+      enable_servos();
+    while (stepper>296)
+    {
+        enable_servos();
+        
+        set_servo_position(0,stepper);
+        stepper=stepper-10;
+        msleep(20);
+    }
+    disable_servos();
+}
+void WINDSHEILDWIPPERYAY()
+{
+    int winddd=0;
+    while (winddd<2)
+    {
+        SERVO_LEFT();
+        SERVO_RIGHT();
+        winddd=winddd+1;
+    }
+}
+void DECAPITATERDOWN()
+{
+int steper=1694;
+    enable_servos();
+    while(steper<2046)
+    {
+       set_servo_position(0,975);
+        set_servo_position(1,steper);
+        steper=steper+50;
+        msleep(100);
+    }
+        disable_servos();                   
+}
+void DECAPITATERUP()
+{
+int steper=2046;
+    enable_servos();
+    while(steper>1694)
+    {
+         set_servo_position(0,975);
+        set_servo_position(1,steper);
+        steper=steper-50;
+        msleep(100);
+    }
+        disable_servos();                   
+}
+void BABYWIPES()
+{  
+    SERVO_RIGHT();
+   SERVO_LEFT();
+}
+    
+    
