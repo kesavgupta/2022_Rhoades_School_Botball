6e89a84d593682feaaa118bee4bab485fa48a224
kesavgupta <101472174+kesavgupta@users.noreply.github.com>
2022-04-09 09:04:02 -0700

    Add files via upload

--- main-2.c
+++ main-2.c
@@ -2,156 +2,60 @@
 
 int main()
 {
-    int STAGE = 1;
-    int ss = 0;
-    // servo steps
-    int sp = 1900;
-    //servo poststion
-    int rc = 0;
-    // range count
-    //set to starting pos
-    enable_servos();
-    set_servo_position(0,1900);
-    set_servo_position(1,1100);
-    set_servo_position(2,801);
-    msleep(150);
-    disable_servos();
-    //stop at ring
+    int COUNTER=0;
 
-
-    motor(2,30);
-    motor(3,30);
-    msleep(1000);
-
-    motor(2,0);
-    motor(3,0);
-    msleep(400);
-    STAGE = 2;
-
-
-
-    if(STAGE==2)
+    create_connect();
+    while(analog(0)<3400) 
+    {create_drive_direct(100,100);} //drive until robot sees black tape
+    create_drive_direct(500,500);
+    msleep(200);
+    while(analog(0)<3400)
+    {create_drive_direct(100,0);} //pivot CW WHILE right sensorsees black tape
+    while(analog(1)<3400)
+    {create_drive_direct(100,0);}//pivot clockwise until left sensor sees black tape
+    while(COUNTER<1000)
     {
-
-        // move forward
-        motor(2,30);
-        motor(3,30);
-        msleep(1000);
-
-
-        STAGE = 3;
-    }
-    // turn
-    while(STAGE == 3)
-    {
-
-        motor(2,30);  
-        motor(3,-30);
-        msleep(2700);
-        motor(2,0);  //
-        motor(3,0);
-        msleep(100);
-        motor(2,40);
-        motor(3,40);
-        msleep(200);
-        STAGE = 4;
-
-    }
-
-    // forward
-
-    if(STAGE == 4) //
-    {
-        motor(2,40);
-        motor(3,40);
-        msleep(9000);
-        // revrse + line up
-        motor(2,-40);
-        motor(3,-40);
-        msleep(2500);
-        motor(2,40);
-        motor(3,-40);
-        msleep(3000);
-        motor(2,-40);
-        motor(3,-40);
-        msleep(2700);
-        STAGE = 5;
-    }
-    while(STAGE == 5)
-    {
-        enable_servos();
-        set_servo_position(1,1450);
-        set_servo_position(2,520);
-        msleep(150);
-        disable_servos();
-        STAGE = 6;
-    }
-    // finding range stage
-    while(STAGE == 6)
-    {
-        if(49>=rc || rc >=0)
+        if(analog(1)>3400)
         {
-            motor(2,-10);
-            motor(3,-10);
-            if(analog(0)>1500)
-            {
-                motor(2,-10);
-                motor(3,-10);
-                msleep(200);
-                STAGE = 7;
-            }
-            else
-            {
-                rc = rc + 1;
-                msleep(30);
-            }
+            create_drive_direct(20,100);
         }
-        if(rc>= 50)
+        else
         {
-            motor(2,10);
-            motor(3,10);
-            if(analog(0)>1500)
-            {
-                motor(2,10);
-                motor(3,10);
-                msleep(300);
-                STAGE = 7;
-            }
-            else
-            {
-                rc = rc - 1;
-                msleep(5);
-            }
+            create_drive_direct(100,20);
         }
+        COUNTER=COUNTER +1;
     }
-    if(STAGE == 7)
+    COUNTER=0;
+     while(COUNTER<7000)
     {
-        motor(2,0);
-        motor(3,0);
-        msleep(10);
-        enable_servos();
-        set_servo_position(1,1600);
-        set_servo_position(2,601);
-        msleep(1150);
-        while(ss < 41)
+        if(analog(0)>3400)
         {
-            ss = ss + 1;
-            sp = sp - 20;
-            set_servo_position(0,sp);
-            msleep(20);
+            create_drive_direct(100,20);
         }
-        set_servo_position(0,1097);
-        msleep(550);
-        set_servo_position(1,1005);
-        set_servo_position(2,1103);
-        msleep(150);
-        disable_servos();
-         set_servo_position(0,1300);
-        msleep(550);
+        else
+        {
+            create_drive_direct(20,100);
+        }
+        COUNTER=COUNTER +1;
     }
+ 
+    /*create_drive_direct(50,-50); //spin CW for some amt of time
+    msleep(100);*/
+    
 
+    /* create_drive_direct(0,-100);
+        msleep(2800);
+        create_drive_direct(-500,-500);
+        msleep(3400);
+        create_drive_direct(-100,-100);
+        msleep(2000);
+    //1Pivot conterclockwise to avoid seeing black tape for _ time//
+    //2Pivot CCW while analog(0)<4000
+    //2aDrive straightfor short amt of time//
+    //3Drive forward while analog(0)<4000*/
+    
+    create_stop();
+    create_disconnect();
 
-
-    printf("Hello World\n");
-    return 0;
+    return(0);
 }
